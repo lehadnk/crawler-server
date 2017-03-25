ddfcfe786d950cca409c3c32ed864440594a57cd
Alexey Zauzin <lehadnk@gmail.com>
2017-03-26 00:00:22 +0300

    Fixed bottom wall on the second map

--- data/maps/another.php
+++ data/maps/another.php
@@ -27,10 +27,10 @@ $creature->behaviour = new \Behaviours\Loaf($creature);
 $this->spawnPoint = [5, 5];
 
 $map = array_fill(1, 20, array_fill(1, 40, ' '));
-foreach ($map as $x => &$row) {
-    foreach ($row as $y => &$cell) {
-        if ($x == 1 || $x == 19 || $y == 1 || $y == 40) {
-            $cell = '#';
+foreach ($map as $x => $row) {
+    foreach ($row as $y => $cell) {
+        if ($x == 1 || $x == 20 || $y == 1 || $y == 40) {
+            $map[$x][$y] = '#';
         }
     }
 }
